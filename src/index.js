d1bfb59ae43d0c16e325880e44f7cc1c3dda62b0
felipeghidini <lipeghidini18@gmail.com>
2023-03-08 14:16:04 -0300

    Add Nodemon ESLint e EditorConfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (resquest, response) => {
-    response.send('Hello World');
-})
+  response.send('Hello World');
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
