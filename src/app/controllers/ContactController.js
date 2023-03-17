56fe79ef8c3da0df59bc30a0b774d5a1603103ef
felipeghidini <lipeghidini18@gmail.com>
2023-03-17 09:25:30 -0300

    Included updated route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -31,7 +31,7 @@ class ContactController {
     }
 
     if (contactExist) {
-      return response.status(400).json({ error: 'This email is alread been taken' });
+      return response.status(400).json({ error: 'This email is alread in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -41,8 +41,31 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params;
+    const { name, email, phone, category_id } = request.body;
+
+    const contactExist = await ContactsRepository.findById(id);
+
+    if (!contactExist) {
+      return response.status(404).json({ error: 'Contact Not Found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
