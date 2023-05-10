9276052f284f9bddd7c1ff16ef6ac3f16a6dc08c
felipeghidini <lipeghidini18@gmail.com>
2023-05-10 06:42:47 -0300

    Aplicando JOIN nas tabelas

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -3,12 +3,21 @@ const db = require('../../database');
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
-    const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
+    const rows = await db.query(`
+      SELECT contacts.*, categories.name AS category_name
+      FROM contacts
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      ORDER BY contacts.name ${direction}
+    `);
     return rows;
   }
 
   async findById(id) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    const [row] = await db.query(`
+      SELECT contacts.*, categories.name AS category_name
+      FROM contacts
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      WHERE contacts.id = $1`, [id]);
     return row;
   }
 
@@ -22,7 +31,12 @@ class ContactsRepository {
     return deleteOp;
   }
 
-  async create({ name, email, phone, category_id }) {
+  async create({
+    name,
+    email,
+    phone,
+    category_id
+  }) {
     const [row] = await db.query(`
       INSERT INTO contacts(name, email, phone, category_id)
       VALUES($1, $2, $3, $4)
@@ -32,7 +46,12 @@ class ContactsRepository {
     return row;
   }
 
-  async update(id, { name, email, phone, category_id }) {
+  async update(id, {
+    name,
+    email,
+    phone,
+    category_id
+  }) {
     const [row] = await db.query(`
     UPDATE contacts
     SET name= $1, email = $2, phone = $3, category_id = $4
