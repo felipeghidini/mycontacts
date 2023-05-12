b4c840411e1c18cf00738e9800c07e312463cb36
felipeghidini <lipeghidini18@gmail.com>
2023-05-12 06:23:26 -0300

    Tratando erros

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { json } = require('express');
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
